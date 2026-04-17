bf92e2781acc750f97adf9af033a2271880a8f4c
j.etienne <j.etienne@gxp.co.jp>
2026-04-17 09:57:09 +0900

    day1 and day2 joined

--- javascript/01/01.js
+++
@@ -1,6 +0,0 @@
-const runButton = document.getElementById('runButton');
-const output = document.getElementById('output');
-
-runButton.addEventListener('click', () => {
-  output.textContent = 'JavaScript demo executed.';
-});
